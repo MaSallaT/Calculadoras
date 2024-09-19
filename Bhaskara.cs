66ba45675ad40ceb88bf7b124fea0455589a50a6
MaSallaT <mayara.salla@aluno.senai.br>
2024-09-19 14:59:45 -0300

    Atividade

---
+++ Bhaskara.cs
@@ -0,0 +1,21 @@
+﻿namespace CalcBhaskara
+{
+    public static class Bhaskara
+    {
+        public static (double?, double?) Raiz(double n1, double n2, double n3)
+        {
+            double Delta = (n2 * n2) - (4 * n1 * n3);
+
+            if (Delta < 0)
+            {
+                return (null, null); 
+            }
+
+            double raizDoDelta = Math.Sqrt(Delta);
+            double x1 = (-n2 + raizDoDelta) / (2 * n1);
+            double x2 = (-n2 - raizDoDelta) / (2 * n1);
+
+            return (x1, x2);
+        }
+    }
+}
